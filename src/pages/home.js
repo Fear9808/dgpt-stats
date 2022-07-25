7488bb83ea380efee81f862bca37e8f2e6018cae
Fear9808 <felix.arnestrand@gmail.com>
2022-07-25 22:01:36 +0200

    Moving back to # instead of picture for now

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ import YearPickerComponent from '../components/YearPickerComponent';
 import BarStatComponent from '../components/BarStatComponent';
 import NumberStatComponent from '../components/NumberStatComponent';
 import RadarChartComponent from '../components/RadarChartComponent';
-import throphy from './../images/prize_cup_orange_60x60.png';
+//import throphy from './../images/prize_cup_orange_60x60.png';
 
 import * as Default from '../style/DefaultValues';
 import layoutStyle from '../style/LayoutStyle.module.css';
@@ -115,8 +115,8 @@ const Home = () => {
                         <div className={layoutStyle.nameGrid}>
                             <PlayerPickerComponent data={players} player={player} handlePlayerSelect={handlePlayerSelect}/>
                             <div className={layoutStyle.positionDisplay}>
-                                <img src={throphy} alt='placement'/>
-                                <h2 className={layoutStyle.positionDisplayNumber} style={{color: Default.PLAYER_COLOR}}>{playerData.playerPosition}</h2>
+                                {/* <img src={throphy} alt='placement'/> */}
+                                <h2 className={layoutStyle.positionDisplayNumber} style={{color: Default.PLAYER_COLOR}}>{'#' + playerData.playerPosition}</h2>
                             </div>
                         </div>
                         
